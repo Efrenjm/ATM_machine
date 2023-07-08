2f8112a65dd2d5aa3aabe9920a2e8df829e2e186
Efren Jimenez <efrenjimenez_96@hotmail.com>
2023-07-07 20:08:48 -0600

    Landing Page completa

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+let carouselElements = document.getElementsByClassName("carousel-item");
+console.log(carouselElements);
+let c = 1;
+// carouselElements[0].style.backgroundImage = "url('https://picsum.photos/1800/500?random=1')";
+// carouselElements[1].style.backgroundImage = "url('https://picsum.photos/1800/500?random=2')";
+// carouselElements[2].style.backgroundImage = "url('https://picsum.photos/1800/500?random=3')";
+
+
 
 var cuentas = [
     {nombre:"", saldo:""},
