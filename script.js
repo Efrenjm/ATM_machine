46ee8d9e8365b8aff7c4407d9908837c1d23a8d2
Efren Jimenez <efrenjimenez_96@hotmail.com>
2023-07-29 07:50:25 -0600

    some more features

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
-let carouselElements = document.getElementsByClassName("carousel-item");
-console.log(carouselElements);
-let c = 1;
-// carouselElements[0].style.backgroundImage = "url('https://picsum.photos/1800/500?random=1')";
-// carouselElements[1].style.backgroundImage = "url('https://picsum.photos/1800/500?random=2')";
-// carouselElements[2].style.backgroundImage = "url('https://picsum.photos/1800/500?random=3')";
-
-
-
-var cuentas = [
-    {nombre:"", saldo:""},
-    {nombre:"", saldo:""},
-    {nombre:"", saldo:""},
+let cuentas = [
+    { 'correo': 'Mali@correo.com', 'contraseña':'pass', 'saldo': 200 },
+    { 'correo': 'Gera@correo.com', 'contraseña':'pass', 'saldo': 290 },
+    { 'correo': 'Maui@correo.com', 'contraseña':'pass', 'saldo': 67 },
 ]
+
+window.onload = function () {
+    let submit = document.getElementById('');
+    let correo = document.getElementById('floatingInput');
+    let pass = document.getElementById('floatingPassword');
+    submit.addEventListener('click',()=>{
+
+    })
+};
+
+
+
+
 
 
